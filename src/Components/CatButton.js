eaad341777907a0a48e57e58e107d50396441f2c
BethT141 <beth.thomas@softwire.com>
2021-12-07 14:10:18 +0000

    new edits

--- src/Components/CatButton.js
+++ src/Components/CatButton.js
@@ -5,12 +5,21 @@ export class CatButton extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {On: false}
         this.handleClick = this.handleClick.bind(this);
     }
 
 
     handleClick() {
-        document.getElementById('catdiv').innerHTML = '<p><img src="./catpic.jpg"></p>';
+        if (this.state.On) {
+            this.setState({On: false});
+            document.getElementById('catdiv').innerHTML = '';
+        }
+        else {
+            this.setState({On: true});
+            document.getElementById('catdiv').innerHTML = '<p><img src="https://petcostumecenter.com/wp-content/uploads/2020/05/straw-sombrero-pet-hat--scaled.jpg" width="200" height="200"></p>';
+        }
+
     }
 
 
